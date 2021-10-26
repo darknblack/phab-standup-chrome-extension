b825a51cd03db64ae84d8ea30bc35c30e63939ae
Ian Ishmael C. Oderon <ianoderon@gmail.com>
2021-10-27 05:38:48 +0800

    Add resizable, improve text

--- content.js
+++ content.js
@@ -4,78 +4,58 @@ let tickets = [];
 let geekbot = '';
 const FORCE_USERNAME = '';
 
-function setupHTML() {
-  const template = `
-<div id="phab-standup-wrapper">
-<div id="header-wrapper">
-  <h2>STANDUP</h2>
-  <div id="status"></div>
-</div>
-<div id="content-wrapper">
-  <div id="phab-standup-content"></div>
-</div>
-<div id="buttons-wrapper">
-  <div id="copy-to-clipboard">Copy to clipboard</div>
-  <div id="reload">Reload</div>
-  <div id="close">Close</div>
-</div>
-</div>
-`;
-  const html = new DOMParser().parseFromString(template, 'text/html').body.childNodes[0];
-  document.body.prepend(html);
-}
-
-function getProfileContents(htmlString) {
-  var div = document.createElement('div');
-  div.innerHTML = htmlString.trim();
-  return div;
-}
-
-function getTitle(htmlString) {
-  var div = document.createElement('div');
-  div.innerHTML = htmlString.trim();
-  const phabTitle = div.querySelector(
-    '.phui-header-view .phui-header-row .phui-header-col2 .phui-header-header'
-  ).innerText;
-  return phabTitle;
-}
-
-function getUserName() {
-  try {
-    const href = document.querySelector('#phabricator-standard-page a:not([href="#"]).phabricator-core-user-menu').href;
-    const testHref = href.match(/\/p\/([a-zA-Z]{4})/);
-    if (testHref) {
-      const cur = testHref[0].replace('/p/', '');
-      return cur;
-    }
-  } catch (e) {
-    console.error(e);
-  }
-  return '';
-}
+const userName = FORCE_USERNAME || getUserName();
 
 function init() {
   setupHTML();
 
-  const userName = FORCE_USERNAME || getUserName();
   const phabStandupContent = document.getElementById('phab-standup-content');
   const standupText = document.getElementById('status');
   const hideBtn = document.getElementById('close');
   const reloadBtn = document.getElementById('reload');
   const wrapper = document.getElementById('phab-standup-wrapper');
   const selectAllBtn = document.getElementById('copy-to-clipboard');
+  const resize = document.getElementById('resize');
+
+  const h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+  function initResize(e) {
+    window.addEventListener('mousemove', doResize, false);
+    window.addEventListener('mouseup', stopResize, false);
+  }
+
+  function doResize(e) {
+    e.preventDefault();
+
+    const newWidth = wrapper.offsetLeft - e.clientX + wrapper.offsetWidth;
+    const newHeight = e.clientY - wrapper.offsetTop;
+
+    if (e.clientX >= 5) {
+      wrapper.style.width = newWidth + 'px';
+    }
+
+    if (h - 10 >= newHeight) {
+      wrapper.style.height = newHeight + 'px';
+    }
+  }
+
+  function stopResize(e) {
+    window.removeEventListener('mousemove', doResize, false);
+    window.removeEventListener('mouseup', stopResize, false);
+  }
+
+  resize.addEventListener('mousedown', initResize, false);
 
   hideBtn.addEventListener('click', () => {
     wrapper.remove();
     isInit = false;
   });
 
-  reloadBtn.addEventListener('click', getStandupFormatted);
+  reloadBtn.addEventListener('click', getStandup);
 
   selectAllBtn.addEventListener('click', () => {
-    const hasHiglight = phabStandupContent.className.includes('highlight');
-
-    if (!hasHiglight) {
+    const isHighlighted = phabStandupContent.className.includes('highlight');
+    if (!isHighlighted) {
       const textField = document.createElement('textarea');
       textField.innerHTML = geekbot;
       phabStandupContent.classList.add('highlight');
@@ -88,7 +68,7 @@ function init() {
     }
   });
 
-  getStandupFormatted();
+  getStandup();
 
   function renderContent(value) {
     if (typeof value === 'string') {
@@ -96,7 +76,8 @@ function init() {
     } else {
       let html = '';
       value.forEach(item => {
-        html += item + '<br />';
+        const [ticket, title] = item.split(':');
+        html += '<b>' + ticket + ':</b>' + title + '<br />';
       });
       phabStandupContent.innerHTML = html;
     }
@@ -106,11 +87,11 @@ function init() {
     standupText.innerHTML = string;
   }
 
-  async function getStandupFormatted() {
+  async function getStandup() {
     tickets = [];
     geekbot = '';
     renderContent('');
-    renderStatus('Getting Phab Tickets...');
+    renderStatus('Getting Phabricator Tickets...');
     phabStandupContent.classList.remove('highlight');
 
     const profileUrl = `https://phab.splitmedialabs.com/p/${userName}`;
@@ -118,7 +99,7 @@ function init() {
 
     if (result.status === 200) {
       const profileHtmlText = await result.text();
-      const ticketrows = getProfileContents(profileHtmlText).getElementsByClassName('phui-feed-story-head');
+      const ticketrows = getProfileHTML(profileHtmlText).getElementsByClassName('phui-feed-story-head');
 
       Array.prototype.slice.call(ticketrows).forEach(item => {
         const value = item.innerHTML.match(/[tT]\d{5}/);
@@ -146,18 +127,80 @@ function init() {
         }
       }
 
-      return renderStatus('Done...');
+      return renderStatus('Complete...');
     }
 
-    renderStatus('Failed to get Phab Tickets...');
+    renderStatus('Failed to get Phabricator Tickets...');
     geekbot = 'Error...';
     renderContent(geekbot);
   }
 }
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.action === 'GET-STANDUP' && !isInit) {
-    init();
-    isInit = true;
+function getProfileHTML(htmlString) {
+  var div = document.createElement('div');
+  div.innerHTML = htmlString.trim();
+  return div;
+}
+
+function getTitle(htmlString) {
+  var div = document.createElement('div');
+  div.innerHTML = htmlString.trim();
+  const phabTitle = div.querySelector(
+    '.phui-header-view .phui-header-row .phui-header-col2 .phui-header-header'
+  ).innerText;
+  return phabTitle;
+}
+
+function getUserName() {
+  try {
+    const href = document.querySelector('#phabricator-standard-page a:not([href="#"]).phabricator-core-user-menu').href;
+    const testHref = href.match(/\/p\/([a-zA-Z]{4})/);
+    if (testHref) {
+      const userID = testHref[0].replace('/p/', '');
+      localStorage.setItem('phab-standup-user-id', userID);
+      return userID;
+    }
+  } catch (e) {
+    console.error(e);
+  }
+
+  const userIDFromLocalStorage = localStorage.getItem('phab-standup-user-id');
+  return userIDFromLocalStorage || '';
+}
+
+function setupHTML() {
+  const template = `
+<div id="phab-standup-wrapper">
+<div id="header-wrapper">
+  <h2>STANDUP</h2>
+  <div id="status"></div>
+</div>
+<div id="content-wrapper">
+  <div id="phab-standup-content"></div>
+</div>
+<div id="buttons-wrapper">
+  <div id="copy-to-clipboard">Copy to clipboard</div>
+  <div id="reload">Reload</div>
+  <div id="close">Close</div>
+</div>
+<div id="resize"></div>
+</div>
+`;
+  const html = new DOMParser().parseFromString(template, 'text/html').body.childNodes[0];
+  document.body.prepend(html);
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === 'GET-STANDUP') {
+    if (!isInit) {
+      init();
+      isInit = true;
+    } else {
+      const psw = document.getElementById('phab-standup-wrapper');
+      if (psw) {
+        psw.remove();
+        isInit = false;
+      }
+    }
   }
 });
